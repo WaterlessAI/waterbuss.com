d460dbcd4dbf6e2173489132bac3c1cdafdf3a0c
WaterlessAI <djmustafa4@gmail.com>
2026-01-28 23:28:21 +0300

    Integrations: Added Google Tag Manager to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NL9M86PB');`
+        }} />
+        {/* End Google Tag Manager */}
         {/* BU SATIR STİLLERİ ZORLA ÇALIŞTIRACAK 👇 */}
         <script src="https://cdn.tailwindcss.com"></script>
         <script dangerouslySetInnerHTML={{
@@ -51,6 +60,16 @@ export default function RootLayout({
         `}</style>
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NL9M86PB"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
         <Analytics />
       </body>
